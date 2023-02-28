3a1668276303798fedcd141a180e944b20c9f397
Jedidiah Oladele <jehddie@gmail.com>
2023-02-28 17:00:06 +0100

    saving questions

--- src/gui.py
+++ src/gui.py
@@ -305,7 +305,7 @@ def b_ask():
         q = text.strip()
         a = resp["text"].strip()
         output_add(q, a)
-        save_question.save_question_to_github(text)
+        save_question.save_question_to_github(q)
 
 
 def b_clear():
