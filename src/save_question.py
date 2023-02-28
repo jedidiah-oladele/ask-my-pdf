0ec7ec8192cedc08bec954892aa3e164fac16c11
Jedidiah Oladele <jehddie@gmail.com>
2023-02-28 17:39:22 +0100

    bug fixes

--- src/save_question.py
+++ src/save_question.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 from github import Github
-from github import GithubException
 
 
 def save_question_to_github(question_text):
@@ -22,7 +21,7 @@ def save_question_to_github(question_text):
         file_content = repo.get_contents(file_path)
         df = pd.read_csv(file_content.download_url)
 
-    except GithubException.UnknownObjectException:
+    except:
         # If the file doesn't exist yet, create it
         df = pd.DataFrame(columns=["questions"])
         buffer = df.to_csv(index=False)
