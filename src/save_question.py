8abd674936f4520be5a17af9edba62530159282e
Jedidiah Oladele <jehddie@gmail.com>
2023-02-28 17:17:39 +0100

    saving questions

--- src/save_question.py
+++ src/save_question.py
@@ -19,7 +19,7 @@ def save_question_to_github(question_text):
         file_content = repo.get_contents(file_path)
         df = pd.read_csv(file_content.download_url)
 
-    except:
+    except KeyError:
         # If the file doesn't exist yet, create it
         df = pd.DataFrame(columns=["questions"])
         buffer = df.to_csv(index=False)
