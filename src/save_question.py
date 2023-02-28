f51a83d45ce41a742045e44a08cc86fab8a2fbd9
Jedidiah Oladele <jehddie@gmail.com>
2023-02-28 18:02:21 +0100

    bug fixes

--- src/save_question.py
+++ src/save_question.py
@@ -6,6 +6,7 @@ from github import Github
 def save_question_to_github(question_text):
 
     file_path = os.path.join("data", "questions.csv")
+    new_question_df = pd.DataFrame.from_records([{"questions": question_text}])
 
     access_token = os.environ["GITHUB_ACCESS_TOKEN"]
     repo_name = os.environ["GITHUB_REPO_NAME"]
@@ -18,23 +19,19 @@ def save_question_to_github(question_text):
     repo = g.get_user(owner_name).get_repo(repo_name)
 
     try:
-        file_content = repo.get_contents(file_path)
-        df = pd.read_csv(file_content.download_url)
-        st.write(df)
+        # If the file exists append to it and update
+        contents = repo.get_contents(file_path)
+        df = pd.concat(
+            [
+                pd.read_csv(contents.download_url),
+                new_question_df,
+            ],
+            ignore_index=True,
+        )
+        buffer = df.to_csv(index=False)
+        repo.update_file(contents.path, "Added question", buffer, contents.sha)
 
     except:
         # If the file doesn't exist yet, create it
-        df = pd.DataFrame(columns=["questions"])
-        buffer = df.to_csv(index=False)
+        buffer = new_question_df.to_csv(index=False)
         repo.create_file(file_path, "Created questions.csv", buffer)
-
-    df = pd.concat(
-        [df, pd.DataFrame.from_records([{"questions": question_text}])],
-        ignore_index=True,
-    )
-    st.write(df)
-    buffer = df.to_csv(index=False)
-
-    # Commit changes to GitHub
-    contents = repo.get_contents(file_path)
-    repo.update_file(contents.path, "Added question", buffer, contents.sha)
