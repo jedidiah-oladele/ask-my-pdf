b5c9f4e57dabf6216275fc4a65acd7c0e52a54f2
Jedidiah Oladele <jehddie@gmail.com>
2023-02-28 17:31:03 +0100

    bug fixes

--- src/save_question.py
+++ src/save_question.py
@@ -11,6 +11,8 @@ def save_question_to_github(question_text):
     repo_name = os.environ["GITHUB_REPO_NAME"]
     owner_name = os.environ["GITHUB_OWNER_NAME"]
 
+    import streamlit as st
+
     # Authenticate with GitHub API
     g = Github(access_token)
     repo = g.get_user(owner_name).get_repo(repo_name)
@@ -19,13 +21,11 @@ def save_question_to_github(question_text):
         file_content = repo.get_contents(file_path)
         df = pd.read_csv(file_content.download_url)
 
-    except:
+    except NameError:
         # If the file doesn't exist yet, create it
         df = pd.DataFrame(columns=["questions"])
         buffer = df.to_csv(index=False)
         repo.create_file(file_path, "Created questions.csv", buffer)
-
-    import streamlit as st
 
     st.write(df)
     buffer = pd.concat(
